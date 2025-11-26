9265cf8eb27f47ca3121dcb9c0789496c88ed760
Morten Hersson <mhersson@gmail.com>
2025-11-26 09:51:03 +0100

    feat(vectorsigma): Update to version 1.2.0

--- Formula/vectorsigma.rb
+++ Formula/vectorsigma.rb
@@ -1,8 +1,8 @@
   class Vectorsigma < Formula
     desc "VectorSigma is a tool for generating finite state machines (FSM) in Go from UML diagrams"
     homepage "https://github.com/mhersson/vectorsigma"
-    url "https://github.com/mhersson/vectorsigma/archive/refs/tags/v1.1.0.tar.gz"
-    sha256 "b150531202a2d142b80ddc216d0e6c99d181d8d3d967c13904fc1226256d3dc0"
+    url "https://github.com/mhersson/vectorsigma/archive/refs/tags/v1.2.0.tar.gz"
+    sha256 "1018d29416674902e211e8bdf0176cc1eabd364b43e7d45306f10f1bc09897e2"
     license "MIT"
     head "https://github.com/mhersson/vectorsigma.git", branch: "main"
 
